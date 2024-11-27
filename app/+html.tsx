e98f991e415e8907010a823fb2ab5594b4f672a0
Samuel Acevedo <acevedosamuel98@gmail.com>
2024-11-27 00:34:35 -0400

    Fixed UI Height on Web mobile

--- app/+html.tsx
+++ app/+html.tsx
@@ -1,5 +1,5 @@
-import { ScrollViewStyleReset } from 'expo-router/html';
-import { type PropsWithChildren } from 'react';
+import { ScrollViewStyleReset } from "expo-router/html";
+import { type PropsWithChildren } from "react";
 
 /**
  * This file is web-only and used to configure the root HTML for every web page during static rendering.
@@ -11,7 +11,7 @@ export default function Root({ children }: PropsWithChildren) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         {/*
           Disable body scrolling on web. This makes ScrollView components work closer to how they do on native.
@@ -32,7 +32,10 @@ const responsiveBackground = `
 body {
   background-color: #fff;
   font-family: 'Poppins', sans-serif;
+  margin: 0;
+  height: 100vh; 
 }
+
 @media (prefers-color-scheme: dark) {
   body {
     background-color: #000;
