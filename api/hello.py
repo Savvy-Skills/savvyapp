baf2c69c25f1b122f99ea01668aaa5164807c684
Samuel Acevedo <acevedosamuel98@gmail.com>
2025-02-25 01:45:45 -0400

    Testing API

--- api/hello.py
+++ api/hello.py
@@ -6,5 +6,9 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+		# Get params from url
+		params = self.path.split("?")[1]
+		params = params.split("&")
+		# Return params as json
+		self.wfile.write(json.dumps(params).encode('utf-8'))
         return
