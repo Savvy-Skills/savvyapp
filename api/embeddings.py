5a491e374f7b9170ac523553fdbf539e2ce372db
Samuel Acevedo <acevedosamuel98@gmail.com>
2025-02-25 23:11:56 -0400

    API hotfix

--- api/embeddings.py
+++ api/embeddings.py
@@ -2,13 +2,13 @@ from http.server import BaseHTTPRequestHandler
 from gensim.models import Word2Vec
 import gensim.downloader
 
-model = gensim.downloader.load("word2vec-google-news-300")
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
+		model = gensim.downloader.load("word2vec-google-news-300")
 		word = self.path.split("/")[-1]
 		embedding = model.wv[word]
 		# convert embedding to json
