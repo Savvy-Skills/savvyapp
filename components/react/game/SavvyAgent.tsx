17a6807e974d5d6af6ec724fe87d85e6d5def20a
Samuel Acevedo <samuel@savvyskills.io>
2025-05-13 01:13:49 -0400

    Game v0.2

--- components/react/game/SavvyAgent.tsx
+++ components/react/game/SavvyAgent.tsx
@@ -1,21 +1,62 @@
 'use dom'
+import { useEffect, useState } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 
 function SavvyAgentGame() {
-  const { unityProvider } = useUnityContext({
-    loaderUrl: ("unity/SavvyAgent.loader.js"),
-    dataUrl: ("unity/SavvyAgent.data"),
-    frameworkUrl: ("unity/SavvyAgent.framework.js"),
-    codeUrl: ("unity/SavvyAgent.wasm"),
-  });
+	const { unityProvider, requestFullscreen } = useUnityContext({
+		loaderUrl: ("unity/SavvyAgent.loader.js"),
+		dataUrl: ("unity/SavvyAgent.data"),
+		frameworkUrl: ("unity/SavvyAgent.framework.js"),
+		codeUrl: ("unity/SavvyAgent.wasm"),
+	});
+	// We'll use a state to store the device pixel ratio.
+	const [devicePixelRatio, setDevicePixelRatio] = useState(
+		window.devicePixelRatio
+	);
+	
 
-  return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <Unity unityProvider={unityProvider} style={{ width: '640px', height: '480px'}} />
-    </div>
-  );
-  ;
+	useEffect(
+		function () {
+			// A function which will update the device pixel ratio of the Unity
+			// Application to match the device pixel ratio of the browser.
+			const updateDevicePixelRatio = function () {
+				setDevicePixelRatio(window.devicePixelRatio);
+			};
+			// A media matcher which watches for changes in the device pixel ratio.
+			const mediaMatcher = window.matchMedia(
+				`screen and (resolution: ${devicePixelRatio}dppx)`
+			);
+			// Adding an event listener to the media matcher which will update the
+			// device pixel ratio of the Unity Application when the device pixel
+			// ratio changes.
+			mediaMatcher.addEventListener("change", updateDevicePixelRatio);
+			return function () {
+				// Removing the event listener when the component unmounts.
+				mediaMatcher.removeEventListener("change", updateDevicePixelRatio);
+			};
+		},
+		[devicePixelRatio]
+	);
+
+	function handleFullscreen() {
+		requestFullscreen(true);
+	}
+
+	return (
+		<div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+			<Unity
+				unityProvider={unityProvider}
+				style={{ width: 1280, height: 720 }}
+				devicePixelRatio={devicePixelRatio}
+			/>
+			<div id="buttons">
+				<button id="fullscreen-button" onClick={handleFullscreen}>Fullscreen</button>
+			</div>
+		</div>
+	);
+
+
 }
 
 export default SavvyAgentGame;
